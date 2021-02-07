491c6ea95c8c3497542dd3a83506e89899980a49
Jose Mendoza <pball626@gmail.com>
2021-02-07 22:29:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)[4]
+  array.insert(4, )[4]
 end
 
 #def retrieve_element_from_index(array, index_number)
