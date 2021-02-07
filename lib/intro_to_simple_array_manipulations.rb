efc29e29d121dab06fa237de4a4e505106b6df44
Jose Mendoza <pball626@gmail.com>
2021-02-07 22:26:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,12 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+  #["raindrops on roses", "whiskers on kittens"].concat["sports cars", "flatiron school"]
+end
+
+def using_insert(array, element)
+  array.insert(element)(4)
+end
+
+def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index = ["blue", "red", "am"]
+  retrieve_element_from_index[2]
